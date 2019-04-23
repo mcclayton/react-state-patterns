b2a4473f2fbbc43fc40ef52c6476fbf9fcb06bc0
Michael Clayton <michael.clayton@procore.com>
2019-04-23 08:26:13 -0700

    Declare React as an external dependency in webpack as it is a peer dependency

--- webpack.config.js
+++ webpack.config.js
@@ -15,4 +15,13 @@ module.exports = {
     libraryTarget: 'umd',
     filename: 'main.js',
   },
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react',
+      umd: 'react',
+    },
+  },
 };
