96bfbc779999e61c2e7b9e18ebe29e542499c439
Michael Clayton <michael.clayton@procore.com>
2019-09-25 09:12:17 -0700

    Throw errors for invalid state hooks

--- src/helpers.js
+++ src/helpers.js
@@ -8,6 +8,12 @@ export const defaultHandlers = (state) => ({
 });
 
 export const wrapStateHook = (stateHook) => (props) => {
+  const hookType = typeof stateHook;
+  if (hookType !== 'function') {
+    throw new StatePatternError(
+      `Attempting to wrap invalid hook. Hook must be a function but received: ${hookType}.`
+    );
+  }
   const retVal = stateHook(props);
   if (!retVal || retVal.constructor !== Object) {
     throw new StatePatternError(
